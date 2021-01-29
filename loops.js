38982e914eeb1ec7829fccf938e0ac6279856890
Erick A <servinfma@gmail.com>
2021-01-29 21:20:01 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -2,9 +2,9 @@ function forLoop(array) {
   var value = "";
   for (var i = 0; i < 25; i++) {
     if (i === 1) {
-      value = "I am 1 strange loop."
+      value = '"I am 1 strange loop."'
     } else {
-      value = `I am ${i} strange loop.`
+      value = '"I am ${i} strange loops."'
     }
     array[i] = value
   }
